c13b9ed93aeb30b1941e25f4572c7704c06a72ba
kastrowalker <victorcsa2002@gmail.com>
2020-03-26 23:36:40 -0300

    Finalizando CRUD

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('helloworld', views.helloWorld),
     path('', views.taskList, name='task-list'),
     path('task/<int:id>', views.taskView, name='task-view'),
+    path('newtask/', views.newTask, name='new-task'),
+    path('edit/<int:id>', views.editTask, name='edit-task'),
+    path('delete/<int:id>', views.deleteTask, name='delete-task'),
     path('yourname/<str:name>', views.yourName, name='your-name')
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,15 +1,67 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from .forms import TaskForm
+from django.contrib import messages
 
 from .models import Task
 
 def taskList(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-created_at')
     return render(request, 'tasks/list.html', {'tasks': tasks})
 
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
+
+def newTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.done = 'doing'
+            task.save()
+
+            messages.info(request, 'Tarefa criada com sucesso.')
+
+            return redirect('/')
+        else:
+            messages.info(request, 'Erro ao cadastrar tarefa.')
+
+            return render(request, 'tasks/addtask.html', {'form': form})
+
+    else:
+        form = TaskForm()
+        return render(request, 'tasks/addtask.html', {'form': form})
+
+def editTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+
+        if form.is_valid():
+            task.save()
+
+            messages.info(request, 'Tarefa editada com sucesso.')
+
+            return redirect('/')
+        else:
+            messages.info(request, 'Erro ao editar tarefa.')
+
+            return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+
+    else:
+        return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+
+def deleteTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+
+    messages.info(request, 'Tarefa deletada com sucesso.')
+
+    return redirect('/')
 
 def helloWorld(request):
     return HttpResponse('Hello World!')
